29027100a3e75f756ec453ef4759a4f17a156f36
dex1e <dex1e5@yandex.ru>
2022-04-26 21:31:02 +0600

    added action

--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
+import { addPostActionCreator, updateNewPostTextActionCreator } from '../../../redux/state';
 import s from './MyPosts.module.css'
 import Post from './Post/Post'
+
 
 const MyPosts = (props) => {
 
     let newPostElement = React.createRef()
 
     let addPost = () => {
-        props.addPost()
+        props.dispatch(addPostActionCreator())
     };
 
     let onPostChange = () => {
         let text = newPostElement.current.value
-        props.updateNewPostText(text)
+        let action = updateNewPostTextActionCreator(text)
+        props.dispatch(action)
     }
 
 
